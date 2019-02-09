d8fd2f3b2bac45cf1f4b8de23ea897175b617658
xwr3ck3dx <gamer_sa5m@hotmail.com>
2019-02-10 01:02:37 +0545

    Added News tabs

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.banner-main').css('margin-top','-'+$('.menu').height());
+    $('.banner-content').height($('.banner-main').height()-8);
+
+    $('.collapse ul').css('line-height',$('.navbar').outerHeight);
+    
+})
+$(window).scroll(function() {
+    if ($(this).scrollTop() >200) { // this refers to window
+       $('.navbar').addClass('scroll-nav');
+      
+    }
+    else{
+        $('.navbar').removeClass('scroll-nav');
+    }
+});
